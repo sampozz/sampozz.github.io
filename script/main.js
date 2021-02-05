1b5b6ebfd4b4474bcaf64f9ca780aa29d5f6ca76
sampozz <samuele.pozzani@gmail.com>
2021-02-05 09:31:22 +0100

    new style

--- script/main.js
+++ script/main.js
@@ -1,32 +1,48 @@
-$(document).ready(function() {
+let skillz = [
+    'coding',
+    'UI/UX',
+    'web',
+    'DevOps',
+    'Git',
+    'Linux',
+    'Windows',
+    'JavaScript',
+    'Python',
+    'docker',
+    'Angular',
+    'database'
+]
 
-    let page = $(".page");
-    
-    window.setInterval(function() {
-        
-            let rad = Math.floor(1 + Math.random() * 5);
-            let div = $('<div></div>');
-            div.css({
-                "position": "fixed",
-                "background-color": "white",
-                "width": rad,
-                "height": rad,
-                "border-radius": 50,
-                "top": Math.random() * screen.height + "px",
-                "left": screen.width + 5 + "px"
-            });
-            div.addClass("moving-div");
-            page.append(div);
-        
-    }, 50);
 
-    window.setInterval(function() {
-        for (let div of document.getElementsByClassName("moving-div")) {
-            let speed = 2;
-            div.style.left = parseInt(div.style.left, 10) - speed + "px";
-            if (parseInt(div.style.left, 10) < 0)
-                div.remove();
-        }
-    }, 1);
+let bts = document.querySelectorAll('.back-text');
 
-});
+for (let bt of bts) {
+    bt.innerHTML = choice(skillz);
+}
+
+gsap.timeline()
+    .from('.bt-1', {x: -300, opacity: 0, duration: 0.5, ease: 'linear'})
+    .to('.bt-1', {x: 500, duration: 4, ease: 'linear'})
+    .to('.bt-1', {x: 800, opacity: 0, duration: 0.5, ease: 'linear', onComplete: replaceSkill})
+    .repeat(-1);
+
+gsap.timeline()
+    .fromTo('.bt-2', {x: 800, opacity: 0}, {x: 500, opacity: 0.1, duration: 0.5, ease: 'linear'})
+    .fromTo('.bt-2', {x: 500}, {x: 0, duration: 4, ease: 'linear'})
+    .to('.bt-2', {x: -300, opacity: 0, duration: 0.5, ease: 'linear', onComplete: replaceSkill})
+    .repeat(-1);
+
+
+function replaceSkill() {
+    let skill = choice(skillz);
+    document.querySelector('.bt-1').innerHTML = skill;
+    do {
+        skill = choice(skillz);
+    } while (skill == document.querySelector('.bt-1').innerHTML)
+    document.querySelector('.bt-2').innerHTML = skill;
+}
+
+function choice(array) {
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
+}
